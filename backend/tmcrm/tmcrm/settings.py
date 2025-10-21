7143da87bed2c73bdde31b971af9f3ab7a1010b3
Lev <kurganovl07@mail.ru>
2025-10-21 18:43:31 +0300

    Реализована отправка кода подтверждения на почту пользователя

--- backend/tmcrm/tmcrm/settings.py
+++ backend/tmcrm/tmcrm/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432',
     }
